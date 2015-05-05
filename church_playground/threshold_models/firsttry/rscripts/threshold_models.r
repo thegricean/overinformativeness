955e0e965b392764648ef4a339d65bc24d428e54
thegricean <judithdegen@gmail.com>
2015-05-05 11:26:33 -0700

    updated readme

--- church_playground/threshold_models/firsttry/rscripts/threshold_models.r
+++ church_playground/threshold_models/firsttry/rscripts/threshold_models.r
@@ -7,12 +7,16 @@ r = read.table("data/modelresults.txt", header=T, sep="\t",quote="")
 nrow(r)
 r$ColorPr = paste(r$ColorPrior,"(c)")
 r$SizePr = paste(r$SizePrior,"(s)")
-r$ColorPrior = factor(x=as.character(r$ColorPr), levels=c("super-left-peak (c)","left-peak (c)","flat (c)","mid-peak (c)","super-mid-peak (c)","right-peak (c)","super-right-peak (c)"))
-r$SizePrior = factor(x=as.character(r$SizePr), levels=c("super-left-peak (s)","left-peak (s)","flat (s)","mid-peak (s)","super-mid-peak (s)","right-peak (s)","super-right-peak (s)"))
+r$MatchedPrior = as.factor(ifelse(r$ColorPrior == r$SizePrior, "matched","not matched"))
+r$ColorPrior = factor(x=as.character(r$ColorPr), levels=c("flat (c)","super-left-peak (c)","left-peak (c)","mid-peak (c)","super-mid-peak (c)","right-peak (c)","super-right-peak (c)"))
+r$SizePrior = factor(x=as.character(r$SizePr), levels=c("flat (s)","super-left-peak (s)","left-peak (s)","mid-peak (s)","super-mid-peak (s)","right-peak (s)","super-right-peak (s)"))
 
 r$ValueCombo = as.factor(paste(r$ColorValue,r$SizeValue))
+r$MatchedObject = as.factor(ifelse(r$ColorValue == r$SizeValue, "matched", "not matched"))
 levels(r$ValueCombo)
 head(r)
+summary(r)
+
 
 rect = data.frame(xmin=c(0,5.5,10.5,15.5,20.5), xmax=c(5.5,10.5,15.5,20.5,25.5), ymin=rep(-Inf,5), ymax=rep(Inf,5), alpha=c(.1, .2, .3, .4, .5))
 
@@ -61,4 +65,16 @@ p=ggplot(scol, aes(x=ValueCombo, y=Probability, color=Utterance, group=Utterance
   theme(axis.text.x=element_text(angle=45, vjust=1,hjust=1))
 ggsave("graphs/uttprobs_colorsilencecosts.pdf",height=15,width=30)
 
+#plot only cases of matched priors and object values
+matched = droplevels(subset(r, MatchedPrior == "matched" & MatchedObject == "matched"))
+nrow(matched)
+head(matched)
 
+gat = matched %>% gather(Utterance, Probability, ProbColor:ProbSilence)
+gat$Cost = factor(x=as.character(gat$Costs), levels=c("uniform","prefer-fewer-words","prefer-fewer-words-and-color"))
+gat$Utterance = gsub("Prob","",gat$Utterance)
+ggplot(gat, aes(x=ValueCombo,y=Probability,color=Utterance,group=Utterance)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(ColorPrior~Cost)
+ggsave("graphs/matched_variance.png",width=12,height=10)
