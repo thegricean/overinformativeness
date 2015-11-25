ed1107491a09b51f01df61930cc88afb9e8e856b
cocolab <cocolab@cocolabs-MacBook-Air.local>
2015-11-24 22:32:13 -0800

    changed timeout back to 1 s

--- Caroline/5_OI_Final_Experiment2/game.server.js
+++ Caroline/5_OI_Final_Experiment2/game.server.js
@@ -72,7 +72,7 @@ printObject(others[0]); // this outputs [0]: id [1]: player
         p.player.instance.emit( 'newRoundUpdate', {user: client.userid} );
       });
       gc.newRound() //$('#chatbox').val() != '')  
-    }, 5000);
+    }, 1000);
     
     break; 
   
