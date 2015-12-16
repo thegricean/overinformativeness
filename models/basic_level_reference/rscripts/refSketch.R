42d39b9ffc226c450e7f448c3fe4d89f32231258
thegricean <judithdegen@gmail.com>
2015-12-16 09:49:55 +0100

    updated basic level model graphs

--- models/basic_level_reference/rscripts/refSketch.R
+++ models/basic_level_reference/rscripts/refSketch.R
@@ -1,110 +1,31 @@
 theme_set(theme_bw(18))
-setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/3_scene_variation/results/")
+setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/basic_level_reference/")
 
 source("rscripts/helpers.r")
-r = read.table("parsed/results.txt",sep=",", header=T,quote="")
-
-r$scenevariation = sapply(strsplit(as.character(r$context),"_"), "[", 1)
-r$experiment = sapply(strsplit(as.character(r$context),"_"), "[", 2)
-r$scenevariation = as.factor(as.character(r$scenevariation))
-r$experiment = as.factor(as.character(r$experiment))
-
-head(r)
-nrow(r)
-names(r)
-summary(r)
-d=r
-
-# gather values
-r = r %>%
-  gather(Utterance,Probability,small_green_tv:red_tv)
-nrow(r)
-summary(r)
-r$speaker.opt = as.factor(as.character(r$speaker.opt))
-# test to make sure all dists sum up to 1
-tmp = as.data.frame(r %>% 
-  group_by(color_fidelity,size_fidelity,type_fidelity,color_cost,size_cost,type_cost,speaker.opt,context) %>% 
-  summarise(sum=sum(Probability)))
-summary(tmp)
-save(r,file="parsed/r.RData")
-
-##################
-# PLOTS OF UTTERANCE PROBABILITY AS A FUNCTION OF SPEAKER OPTIMALITY (COLOR), INCREASING COST OF SIZE (COLUMNS) AND INCREASING COST OF COLOR (ROWS) FOR FIXED VALUES OF OBJECT, COLOR FIDELITY, AND SIZE FIDELITY
-
-### PLOT JUST THE RANGES THAT ARE RELEVANT
-# COLOR COST .1
-# SIZE COST .1
-# SPEAKER OPTIMALITY 15
-# COLOR FIDELITY .8
-# SIZE FIDELITY .999
-# TYPE COST .1
-
-toplot = r[r$object == "o1" & r$color_fidelity == .999 & r$size_fidelity == .8  & r$color_cost == .1 & r$size_cost == .1 & r$type_cost == .1 & r$speaker.opt %in% c("10","15","18"),]
-tmp = toplot %>% 
-  group_by(Utterance) %>%
-  summarise(MaxProb = max(Probability))
-tmp = as.data.frame(droplevels(tmp[tmp$MaxProb>.05,]))
-nrow(tmp)
-nrow(toplot)
-
-toplot = droplevels(subset(toplot, Utterance %in% as.character(tmp$Utterance)))
-toplot[toplot$Probability < .0001,]$Probability = NA
-toplot = na.omit(toplot)
-nrow(toplot)
-toplot$Expression = factor(x=as.character(toplot$Utterance),levels=c("couch","small_couch","blue_couch","small_blue_couch","small_chair","small_brown_chair","fan","big_fan","green_fan","big_green_fan","big_chair","big_red_chair"))
-toplot$Context = factor(x=as.character(toplot$context),levels=c("lowvariation_exp1","lowvariation_exp2","highvariation_exp1","highvariation_exp2"))
-
-ggplot(toplot,aes(x=Expression,y=Probability,color=speaker.opt,group=speaker.opt)) +
+d = read.csv("modelOutput_dog.csv",quote="")
+head(d)
+str(d)
+d$Utterance = factor(x=ifelse(d$label == "basicLevel","basic",ifelse(d$label == "superDomain","super","sub")),levels=c("sub","basic","super"))
+  
+ggplot(d, aes(x=Utterance,y=prob,color=as.factor(priorWeight))) +
   geom_point() +
-  geom_line() +
-  facet_grid(type_fidelity~Context,scales="free_x") +
-  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-ggsave("graphs/cf.999_sf.8_cstcost.1.jpg",width=10,height=8)
+  facet_wrap(~condition)
+ggsave("graphs/uttprob-bycondition-byutterance.pdf")
 
+ggplot(d, aes(x=condition,y=prob,color=as.factor(priorWeight))) +
+  geom_point() +
+  facet_wrap(~Utterance)
+ggsave("graphs/uttprob-byutterance-bycondition.pdf",width=12)
 
-# plot only exp. 1 for only one set of settings
-toplot = r[r$object == "o1" & r$color_fidelity == .999 & r$size_fidelity == .8  & r$color_cost == .1 & r$size_cost == .1 & r$type_cost == .1 & r$speaker.opt %in% c("10","15","18") & r$type_fidelity %in% c(.9,.999) & r$context %in% c("lowvariation_exp1","highvariation_exp1") & r$speaker.opt==15,]
-tmp = toplot %>% 
-  group_by(Utterance) %>%
-  summarise(MaxProb = max(Probability))
-tmp = as.data.frame(droplevels(tmp[tmp$MaxProb>.05,]))
-nrow(tmp)
-nrow(toplot)
+agr = d %>% 
+  group_by(Utterance,condition,priorWeight) %>%
+  summarise(MeanProb=mean(prob),ci.low=ci.low(prob),ci.high=ci.high(prob))
+agr = as.data.frame(agr)
+agr$YMin = agr$MeanProb - agr$ci.low
+agr$YMax = agr$MeanProb + agr$ci.high
 
-toplot = droplevels(subset(toplot, Utterance %in% as.character(tmp$Utterance)))
-toplot[toplot$Probability < .0001,]$Probability = NA
-toplot = na.omit(toplot)
-nrow(toplot)
-toplot$Utterance = factor(x=as.character(toplot$Utterance),levels=c("couch","small_couch","blue_couch","small_blue_couch","small_chair","small_brown_chair","fan","big_fan","green_fan","big_green_fan","big_chair","big_red_chair"))
-toplot$Context = toplot$context
-
-ggplot(toplot,aes(x=Utterance,y=Probability)) +
-  geom_bar(stat="identity") +
-  facet_grid(type_fidelity~Context,scales="free_x") +
-  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-ggsave("graphs/low_high_exp1_tfidelity.9.pdf",width=5,height=5)
-
-# plot exp. 1 and exp. 2 for only one set of settings
-toplot = r[r$object == "o1" & r$color_fidelity == .999 & r$size_fidelity == .8  & r$color_cost == .1 & r$size_cost == .1 & r$type_cost == .1 & r$type_fidelity == .9 & r$speaker.opt==15,]
-tmp = toplot %>% 
-  group_by(Utterance) %>%
-  summarise(MaxProb = max(Probability))
-tmp = as.data.frame(droplevels(tmp[tmp$MaxProb>.05,]))
-nrow(tmp)
-nrow(toplot)
-
-toplot = droplevels(subset(toplot, Utterance %in% as.character(tmp$Utterance)))
-toplot[toplot$Probability < .0001,]$Probability = NA
-toplot = na.omit(toplot)
-nrow(toplot)
-#toplot$Utterance = factor(x=as.character(toplot$Utterance),levels=c("couch","small_couch","blue_couch","small_blue_couch","small_chair","small_brown_chair","fan","big_fan","green_fan","big_green_fan","big_chair","big_red_chair"))
-toplot$Exp = as.factor(ifelse(toplot$context %in% c("highvariation_exp1","lowvariation_exp1"),"Exp. 1","Exp. 2"))
-toplot$Variation = as.factor(ifelse(toplot$context %in% c("highvariation_exp1","highvariation_exp2"),"high_variation","low_variation"))
-
-ggplot(toplot,aes(x=Utterance,y=Probability)) +
-  geom_bar(stat="identity") +
-  facet_grid(Exp~Variation,scales="free_x") +
-  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
-ggsave("graphs/low_high_exp1_exp2_tfidelity.9.pdf",width=5,height=5)
-
-
+ggplot(agr, aes(x=condition,y=MeanProb,color=as.factor(priorWeight))) +
+  geom_point() +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
+  facet_wrap(~Utterance)
+ggsave("graphs/uttprob-byutterance-bycondition-errbars.pdf",width=12)
