075ca09d95df63bd67bfca0f503fdcf4e0e83968
caroline <cgraf@uni-osnabrueck.de>
2016-01-05 00:22:20 +0100

    added some more graphs and writing

--- models/basic_level_reference/rscripts/refSketch.R
+++ models/basic_level_reference/rscripts/refSketch.R
@@ -1,9 +1,9 @@
 theme_set(theme_bw(18))
-setwd("/Users/cocolab/overinformativeness/models/basic_level_reference/")
+setwd("/home/caroline/cocolab/overinformativeness/models/basic_level_reference/")
 #setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/basic_level_reference/")
 
-source("rscripts/helpers.r")
-d = read.csv("modelOutput_priorWeight0.05/modelOutput_car_0.05.csv",quote="")
+source("rscripts/helpers.R")
+d = read.csv("modelOutput_antiSuperBias_car_0.05.csv",quote="")
 head(d)
 str(d)
 d$Utterance = factor(x=ifelse(d$label == "basicLevel","basic",ifelse(d$label == "superDomain","super","sub")),levels=c("sub","basic","super"))
@@ -11,12 +11,12 @@ d$Utterance = factor(x=ifelse(d$label == "basicLevel","basic",ifelse(d$label == 
 ggplot(d, aes(x=Utterance,y=prob,color=as.factor(priorWeight))) +
   geom_point() +
   facet_wrap(~condition)
-ggsave("graphs/priorWeight_0.05/uttprob-bycondition-byutterance_car.pdf",width=12)
+ggsave("graphs/antiSuperBias_pW_0.05/antiSuperBias_uttprob-bycondition-byutterance_car_0.05.pdf",width=12)
 
 ggplot(d, aes(x=condition,y=prob,color=as.factor(priorWeight))) +
   geom_point() +
   facet_wrap(~Utterance)
-ggsave("graphs/priorWeight_0.05/uttprob-byutterance-bycondition_car.pdf",width=12)
+ggsave("graphs/antiSuperBias_pW_0.05/antiSuperBias_uttprob-byutterance-bycondition_car_0.05.pdf",width=12)
 
 agr = d %>% 
   group_by(Utterance,condition,priorWeight) %>%
@@ -29,7 +29,7 @@ ggplot(agr, aes(x=Utterance,y=MeanProb,color=as.factor(priorWeight))) +
   geom_point() +
   geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
   facet_wrap(~condition)
-ggsave("graphs/priorWeight_0.05/uttprob-bycondition-byutterance-errbars_car.pdf",width=12)
+ggsave("graphs/antiSuperBias_pW_0.05/antiSuperBias_uttprob-bycondition-byutterance-errbars_car_0.05.pdf",width=12)
 
 agr = d %>% 
   group_by(Utterance,condition,priorWeight) %>%
@@ -42,7 +42,7 @@ ggplot(agr, aes(x=condition,y=MeanProb,color=as.factor(priorWeight))) +
   geom_point() +
   geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
   facet_wrap(~Utterance)
-ggsave("graphs/priorWeight_0.05/uttprob-byutterance-bycondition-errbars_car.pdf",width=12)
+ggsave("graphs/antiSuperBias_pW_0.05/antiSuperBias_uttprob-byutterance-bycondition-errbars_car_0.05.pdf",width=12)
 
 
 
