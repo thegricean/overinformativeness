f64e005befff8e1e0e2e8eb324d425889f238d73
thegricean <judithdegen@gmail.com>
2016-02-16 10:03:02 -0800

    added scene variation experiment replication results

--- experiments/7_overinf_basiclevel_biggersample/results/rscripts/numdistractors.r
+++ experiments/7_overinf_basiclevel_biggersample/results/rscripts/numdistractors.r
@@ -1,38 +1,24 @@
 theme_set(theme_bw(18))
-<<<<<<< HEAD
 setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/experiments/7_overinf_basiclevel_biggersample/results")
 source("rscripts/helpers.r")
 
-d = read.table(file="data/results.csv",sep="\t", header=T, quote="")
-=======
-setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/experiments/4_numdistractors_basiclevel_newitems/results")
-source("rscripts/helpers.r")
-
-d = read.table(file="data/results_modified.csv",sep=",", header=T, quote="")
->>>>>>> e317e02081b3088e9160b391d825bc7c3e169a4e
+d = read.table(file="data/colsizeCor_manModified.csv",sep=",", header=T, quote="")
 
 d$Half = as.factor(ifelse(d$roundNum < 37, "first","second"))
 d$Quarter = as.factor(ifelse(d$roundNum < 19, "first",ifelse(d$roundNum < 37,"second", ifelse(d$roundNum < 55, "third","fourth"))))
 head(d)
-nrow(d)
-#d$speakerMessages
+nrow(d) # 2138 cases
+unique(d$listenerMessages)
+
 summary(d)
 totalnrow = nrow(d)
 totalnrow
-d[is.na(d$typeMentioned),]$typeMentioned = FALSE
 d$TypeMentioned = d$typeMentioned
 d[is.na(d$otherFeatureMentioned),]$otherFeatureMentioned = FALSE
-d[is.na(d$superClassMentioned),]$superClassMentioned = FALSE
-d[is.na(d$basiclevelMentioned),]$basiclevelMentioned = FALSE
-d[is.na(d$superclassattributeMentioned),]$superclassattributeMentioned = FALSE
-d[is.na(d$utteranceContracted),]$utteranceContracted = FALSE
 
 # look at turker comments
-<<<<<<< HEAD
 comments = read.table(file="../mturk/overinf.csv",sep=",", header=T, quote="")
-=======
-comments = read.table(file="data/overinf.csv",sep=",", header=T, quote="")
->>>>>>> e317e02081b3088e9160b391d825bc7c3e169a4e
+
 unique(comments$comments)
 
 ggplot(comments, aes(ratePartner)) +
@@ -53,67 +39,32 @@ table(d$condition,d$targetStatusClickedObj)
 # chisq.test(table(d$condition,d$targetStatusClickedObj))
 
 # how many unique pairs?
-length(levels(d$gameid)) # 28
+length(levels(d$gameid)) # 64
 
 table(d$condition,d$TypeMentioned)
-table(d$condition,d$superClassMentioned)
-table(d$condition,d$superSuperClassMentioned)
-table(d$condition,d$basiclevelMentioned)
-d[d$superclassattributeMentioned == TRUE,]
-
-table(d$condition,d$colorMentioned)
 table(d$condition,d$sizeMentioned)
+table(d$condition,d$colorMentioned)
 
 ### ANALYZE ONLY TRIALS WHERE COLOR/SIZE/NUMDISTRACTORS WAS MANIPULATED
 # exclude pair where listener always seemed to click something completely different
-d = droplevels(d[d$trialType == "colorSizeTrial",])
-totalnrow = nrow(d)
-<<<<<<< HEAD
 d$SufficientProperty = as.factor(ifelse(d$condition %in% c("size21", "size22", "size31", "size32", "size33", "size41", "size42", "size43", "size44"), "size", "color"))
 d$NumDistractors = ifelse(d$condition %in% c("size21","size22","color21","color22"), 2, ifelse(d$condition %in% c("size31","size32","size33","color31","color32","color33"),3,4))
 d$NumDiffDistractors = ifelse(d$condition %in% c("size22","color22","size33","color33","size44","color44"), 0, ifelse(d$condition %in% c("size21","color21","size32","color32","size43","color43"), 1, ifelse(d$condition %in% c("size31","color31","size42","color42"),2,ifelse(d$condition %in% c("size41","color41"),3, 4))))
 d$NumSameDistractors = ifelse(d$condition %in% c("size21","size31","size41","color21","color31","color41"), 1, ifelse(d$condition %in% c("size22","size32","size42","color22","color32","color42"), 2, ifelse(d$condition %in% c("size33","color33","size43","color43"),3,ifelse(d$condition %in% c("size44","color44"),4,NA))))
-=======
-d$SufficientProperty = as.factor(ifelse(d$condition %in% c("sizeOnly4Distr3Same", "sizeOnly2Distr1Same", "sizeOnly4Distr1Same", "sizeOnly3Distr3Same", "sizeOnly3Distr2Same", "sizeOnly3Distr1Same"), "size", "color"))
-d$NumDistractors = ifelse(d$condition %in% c("sizeOnly2Distr1Same","colorOnly2Distr1Same"), 2, ifelse(d$condition %in% c("sizeOnly4Distr1Same","sizeOnly4Distr3Same","colorOnly4Distr1Same","colorOnly4Distr3Same"),4,3))
-d$NumDiffDistractors = ifelse(d$condition %in% c("sizeOnly3Distr3Same","colorOnly3Distr3Same","sizeOnly2Distr2Same","colorOnly2Distr2Same","sizeOnly4Distr4Same","colorOnly4Distr4Same"), 0, ifelse(d$condition %in% c("sizeOnly3Distr2Same","colorOnly3Distr2Same","sizeOnly2Distr1Same","colorOnly2Distr1Same","sizeOnly4Distr3Same","colorOnly4Distr3Same"), 1, ifelse(d$condition %in% c("sizeOnly4Distr2Same","colorOnly4Distr2Same","sizeOnly3Distr1Same","colorOnly3Distr1Same"),2,ifelse(d$condition %in% c("sizeOnly4Distr1Same","colorOnly4Distr1Same"),3, 4))))
-d$NumSameDistractors = ifelse(d$condition %in% c("sizeOnly3Distr1Same","colorOnly3Distr1Same","sizeOnly2Distr1Same","colorOnly2Distr1Same","sizeOnly4Distr1Same","colorOnly4Distr1Same"), 1, ifelse(d$condition %in% c("sizeOnly3Distr2Same","colorOnly3Distr2Same","sizeOnly2Distr2Same","colorOnly2Distr2Same","sizeOnly4Distr2Same","colorOnly4Distr2Same"), 2, ifelse(d$condition %in% c("sizeOnly4Distr3Same","colorOnly4Distr3Same","sizeOnly3Distr3Same","colorOnly3Distr3Same"),3,ifelse(d$condition %in% c("sizeOnly4Distr4Same","colorOnly4Distr4Same"),4,NA))))
->>>>>>> e317e02081b3088e9160b391d825bc7c3e169a4e
 
-d = droplevels(d[d$targetStatusClickedObj == "target",])
+#d = droplevels(d[d$targetStatusClickedObj == "target",])
 
 print(paste("percentage of excluded trials because distractor was chosen: ", (totalnrow -nrow(d))*100/totalnrow))
 
-<<<<<<< HEAD
-# d1=d
-# d1$Experiment = "2"
-# load("../../3_numdistractors_basiclevel/results/data/d.RData") # load first round of data
-# d2=d
-# d2$Experiment = "1"
-
-#d = merge(d1,d2,all=T)
 summary(d)
 
 targets = d
-nrow(targets) # 2143 cases
-targets$UtteranceType = as.factor(ifelse(targets$sizeMentioned == "True" & targets$colorMentioned == "True", "size and color", ifelse(targets$sizeMentioned == "True", "size", ifelse(targets$colorMentioned == "True", "color","OTHER"))))
-=======
-d1=d
-d1$Experiment = "2"
-load("../../3_numdistractors_basiclevel/results/data/d.RData") # load first round of data
-d2=d
-d2$Experiment = "1"
-
-d = merge(d1,d2,all=T)
-summary(d)
-
-targets = d
-nrow(targets) # 998 cases
+nrow(targets) # 2138 cases
 targets$UtteranceType = as.factor(ifelse(targets$sizeMentioned & targets$colorMentioned, "size and color", ifelse(targets$sizeMentioned, "size", ifelse(targets$colorMentioned, "color","OTHER"))))
->>>>>>> e317e02081b3088e9160b391d825bc7c3e169a4e
-targets = droplevels(targets[!is.na(targets$UtteranceType),])
+
+#targets = droplevels(targets[!is.na(targets$UtteranceType),])
 targets[targets$UtteranceType == "OTHER",c("gameid","refExp","condition")]
-table(targets[targets$UtteranceType == "OTHER",]$gameid)
+table(targets[targets$UtteranceType == "OTHER",]$gameid) 
 targets$Color = ifelse(targets$UtteranceType == "color",1,0)
 targets$Size = ifelse(targets$UtteranceType == "size",1,0)
 targets$SizeAndColor = ifelse(targets$UtteranceType == "size and color",1,0)
@@ -128,11 +79,7 @@ agr = targets %>%
   select(sizeMentioned,colorMentioned,typeMentioned,condition) %>%
   gather(Feature,Mentioned,-condition)
 agr$Feature = gsub("Mentioned","",as.character(agr$Feature))
-<<<<<<< HEAD
-agr = droplevels(subset(agr,Mentioned == "True"))
-=======
 agr = droplevels(subset(agr,Mentioned == "TRUE"))
->>>>>>> e317e02081b3088e9160b391d825bc7c3e169a4e
 head(agr)
 
 ggplot(agr, aes(x=Feature)) +
@@ -257,7 +204,8 @@ ggplot(agr, aes(x=RatioOfDiffToSame,y=Probability,color=Distractors,group=1)) +
   xlab("Ratio of distractors with different to same insufficient feature value") +
   ylim(c(0,1)) +
   facet_wrap(~RedundantProperty)
-ggsave("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/talks/2016/edinburgh/model-empirical.pdf",width=8,height=3.5)
+#ggsave("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/talks/2016/edinburgh/model-empirical.pdf",width=8,height=3.5)
+
 
 agr = targets[targets$CorrectProperty == 1,] %>%
   select(redundant,SufficientProperty,NumDistractors,RatioOfDiffToSame) %>%
@@ -283,45 +231,8 @@ ggplot(agr, aes(x=RatioOfDiffToSame,y=Probability,color=Distractors,group=1)) +
   ylab("Probability of redundancy")
 ggsave("graphs_numdistractors/model-empirical_correctonly.pdf",width=9,height=4)
 ggsave("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/writing/2016/theory/pics/model-empirical.pdf",width=9,height=6)
-ggsave("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/talks/2016/edinburgh/model-and-empirical.pdf",width=7.5,height=5)
+#ggsave("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/talks/2016/edinburgh/model-and-empirical.pdf",width=7.5,height=5)
 
-<<<<<<< HEAD
-=======
-# plot by ratio and numdistractors and EXPERIMENT, only for 'correct' properties, to see whether there are differences in the 2-1, 4-1, and 4-3 conditions across experiments
-targets$CorrectProperty = ifelse(targets$SufficientProperty == "color" & (targets$Color == 1 | targets$SizeAndColor == 1), 1, ifelse(targets$SufficientProperty == "size" & (targets$Size == 1 | targets$SizeAndColor == 1), 1, 0)) # 20 cases of incorrect property mention
-targets$minimal = ifelse(targets$SizeAndColor == 0, 1, 0)
-targets$redundant = ifelse(targets$SizeAndColor == 1, 1, 0)
-agr = targets[targets$CorrectProperty == 1,] %>%
-  select(redundant,SufficientProperty,NumDistractors,RatioOfDiffToSame,Experiment) %>%
-  gather(Utterance,Mentioned,-SufficientProperty,-NumDistractors,-RatioOfDiffToSame,-Experiment) %>%
-  group_by(Utterance,SufficientProperty,NumDistractors,RatioOfDiffToSame,Experiment) %>%
-  summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned))
-agr = as.data.frame(agr)
-agr$YMin = agr$Probability - agr$ci.low
-agr$YMax = agr$Probability + agr$ci.high
-agr$Distractors = as.factor(agr$NumDistractors)
-agr$RedundantProperty = ifelse(agr$SufficientProperty == 'color',"size redundant","color redundant")
-ggplot(agr, aes(x=RatioOfDiffToSame,y=Probability,color=Distractors,shape=Experiment,group=1)) +
-  geom_point() +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax)) +
-  xlab("Ratio of different to same (other) feature values") +
-  ylab("Probability of redundancy") +
-  #  geom_smooth(method="lm") +
-  facet_wrap(~RedundantProperty)
-ggsave("graphs_numdistractors/utterancetype_by_condition_byexperiment_ratio_correctonly.pdf",width=7,height=3.2)
-
->>>>>>> e317e02081b3088e9160b391d825bc7c3e169a4e
-
-
-centered=cbind(targets,myCenter(targets[,c("SufficientProperty","RatioOfDiffToSame","NumDistractors")]))
-m = glmer(redundant ~ cSufficientProperty * cRatioOfDiffToSame + cNumDistractors + (1+cSufficientProperty|gameid) + (1|clickedType), data=centered,family='binomial')
-summary(m)
-<<<<<<< HEAD
-m = glmer(redundant ~ SufficientProperty * cRatioOfDiffToSame - cRatioOfDiffToSame + (1|gameid), data=centered,family='binomial')
-=======
-m = glmer(redundant ~ SufficientProperty * cRatioOfDiffToSame - cRatioOfDiffToSame + (1+cSufficientProperty|gameid), data=centered,family='binomial')
->>>>>>> e317e02081b3088e9160b391d825bc7c3e169a4e
-summary(m)
 
 # plot by ratio only
 agr = targets %>%
@@ -338,6 +249,7 @@ ggplot(agr, aes(x=RatioOfDiffToSame,y=Probability)) +
   geom_errorbar(aes(ymin=YMin,ymax=YMax)) +
   facet_grid(SufficientProperty~Utterance)
 ggsave("graphs_numdistractors/utterancetype_by_condition_ratioonly.pdf",width=9,height=4)
+
 ######### ANALYSIS ###########
 
 
@@ -351,6 +263,37 @@ contrasts(centered$SufficientProperty)
 pairscor.fnc(centered[,c("redUtterance","SufficientProperty","NumDistractors","NumSameDistractors","RatioOfDiffToSame")])
 m = glmer(redUtterance ~ cSufficientProperty*cRatioOfDiffToSame + (1+cRatioOfDiffToSame|gameid) + (1|Item), data=centered, family="binomial")
 summary(m)
+
+m.simple = glmer(redUtterance ~ SufficientProperty*cRatioOfDiffToSame - cRatioOfDiffToSame + (1|gameid) + (1|Item), data=centered, family="binomial")
+summary(m.simple)
+
+m.intercepts = glmer(redUtterance ~ cSufficientProperty*cRatioOfDiffToSame + (1|gameid) + (1|Item), data=centered, family="binomial")
+summary(m.intercepts)
+
+# do the analysis only on those cases that have a ratio > 0
+centered = cbind(t[t$RatioOfDiffToSame > 0,], myCenter(t[t$RatioOfDiffToSame > 0,c("SufficientProperty","NumDistractors","NumSameDistractors","roundNum","RatioOfDiffToSame")]))
+contrasts(centered$redUtterance)
+contrasts(centered$SufficientProperty)
+
+pairscor.fnc(centered[,c("redUtterance","SufficientProperty","NumDistractors","NumSameDistractors","RatioOfDiffToSame")])
+m = glmer(redUtterance ~ cSufficientProperty*cRatioOfDiffToSame + (1+cRatioOfDiffToSame|gameid) + (1|Item), data=centered, family="binomial")
+summary(m) # doing the analysis only on the ratio > 0 cases gets rid of the interaction, ie variation has the same effect on color-redundant and size-redunant trials. (that is, the big scene variation slop in hte color-redundant condition was driven mostly by the 0-ratio cases)
+
+m.intercepts = glmer(redUtterance ~ cSufficientProperty*cRatioOfDiffToSame + (1|gameid) + (1|Item), data=centered, family="binomial")
+summary(m.intercepts)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
