7a4356798f3711671aad19c98c1df1850d4294e0
caroline <cgraf@uni-osnabrueck.de>
2016-05-31 19:32:28 +0200

    model exploration, extract typ,freq,len data

--- models/basic_level_reference/modelExploration/log_unlog_litlisOutput.r
+++ models/basic_level_reference/modelExploration/log_unlog_litlisOutput.r
@@ -1,21 +1,21 @@
 theme_set(theme_bw(18))
 
-# setwd("/home/caroline/cocolab/overinformativeness/models/basic_level_reference/modelExploration")
-setwd("~/cogsci/projects/stanford/projects/overinformativeness/models/basic_level_reference/modelExploration")
+setwd("/home/caroline/cocolab/overinformativeness/models/basic_level_reference/modelExploration")
+#setwd("~/cogsci/projects/stanford/projects/overinformativeness/models/basic_level_reference/modelExploration")
 
 source("rscripts/helpers.R")
 
 #load("data/r.RData")
-unlogged = read.table(file="modelOutput_unloggedTyps.csv",sep=",", header=T, quote="")
+unlogged = read.table(file="litLisOutput_unloggedTyps.csv",sep=",", header=T, quote="")
 head(unlogged)
-logged = read.table(file="modelOutput_loggedTyps.csv",sep=",", header=T, quote="")
+logged = read.table(file="litLisOutput_loggedTyps.csv",sep=",", header=T, quote="")
 head(logged)
 nrow(logged)
 
 agr = unlogged %>%
-  select(p_l_sub_t,p_l_sub_d1,p_l_sub_d2, t_sub_d1, t_sub_t) %>%
-  gather(Utterance,Mentioned,-t_sub_t, -t_sub_d1) %>%
-  group_by(Utterance,t_sub_t, t_sub_d1) %>%
+  select(p_l_t,p_l_d1,p_l_d2, t_d1, t_t) %>%
+  gather(Utterance,Mentioned,-t_t, -t_d1) %>%
+  group_by(Utterance,t_t, t_d1) %>%
   summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned))
 agr = as.data.frame(agr)
 agr$YMin = agr$Probability - agr$ci.low
@@ -24,12 +24,12 @@ agr$YMax = agr$Probability + agr$ci.high
 ggplot(agr, aes(x=Utterance,y=Probability)) +
   geom_bar(stat="identity") +
   geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
-  facet_grid(t_sub_d1~t_sub_t) +
+  facet_grid(t_d1~t_t) +
   #facet_wrap(~condition) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
-ggsave("litLisProb_by_targetTyp_by_t_sub_d1.pdf",width=10,height=10)
+ggsave("litLisProb_unlogged_by_t_t_by_t_d.pdf",width=10,height=10)
 
-ggplot(agr, aes(x=t_sub_t,y=Probability,color=t_sub_d1,group=t_sub_d1)) +
+ggplot(agr, aes(x=t_t,y=Probability,color=t_d1,group=t_d1)) +
 #   geom_bar(stat="identity") +
   geom_point() +
   geom_line() +
@@ -37,14 +37,14 @@ ggplot(agr, aes(x=t_sub_t,y=Probability,color=t_sub_d1,group=t_sub_d1)) +
   facet_wrap(~Utterance) +
   #facet_wrap(~condition) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
-ggsave("litLisProb_by_targetTyp_by_t_sub_d1.pdf",width=10,height=10)
+ggsave("litLisProb2_unlogged_by_t_t_by_t_d.pdf",width=10,height=10)
 
 
 # do same thing for logged typicalities
 agr = logged %>%
-  select(p_l_sub_t,p_l_sub_d1,p_l_sub_d2, t_sub_d1, t_sub_t) %>%
-  gather(Utterance,Mentioned,-t_sub_t, -t_sub_d1) %>%
-  group_by(Utterance,t_sub_t, t_sub_d1) %>%
+  select(p_l_t,p_l_d1,p_l_d2, t_d1, t_t) %>%
+  gather(Utterance,Mentioned,-t_t, -t_d1) %>%
+  group_by(Utterance,t_t, t_d1) %>%
   summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned))
 agr = as.data.frame(agr)
 agr$YMin = agr$Probability - agr$ci.low
@@ -53,12 +53,12 @@ agr$YMax = agr$Probability + agr$ci.high
 ggplot(agr, aes(x=Utterance,y=Probability)) +
   geom_bar(stat="identity") +
   geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
-  facet_grid(t_sub_d1~t_sub_t) +
+  facet_grid(t_d1~t_t) +
   #facet_wrap(~condition) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
-ggsave("litLisProb_by_targetTyp_by_t_sub_d1.pdf",width=10,height=10)
+ggsave("litLisProb_logged_by_t_t_by_t_d.pdf",width=10,height=10)
 
-ggplot(agr, aes(x=t_sub_t,y=Probability,color=t_sub_d1,group=t_sub_d1)) +
+ggplot(agr, aes(x=t_sub_t,y=Probability,color=t_d1,group=t_d1)) +
   #   geom_bar(stat="identity") +
   geom_point() +
   geom_line() +
@@ -66,13 +66,13 @@ ggplot(agr, aes(x=t_sub_t,y=Probability,color=t_sub_d1,group=t_sub_d1)) +
   facet_wrap(~Utterance) +
   #facet_wrap(~condition) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
-ggsave("litLisProb_by_targetTyp_by_t_sub_d1.pdf",width=10,height=10)
+ggsave("litLisProb2_logged_by_targetTyp_by_t_sub_d1.pdf",width=10,height=10)
 
 
 
-# t_sub_t = targetTypicality for sub term
-# t_sub_d1 = distr1Typicality (=distr2Typicality) for sub term
-# p_l_sub_t = literal listener probability to pick target based on sub term
-# p_l_sub_d1 = literal listener probability to pick distractor1 based on sub term
-# p_l_sub_d2 = literal listener probability to pick distractor2 based on sub term
+# t_t = targetTypicality 
+# t_d1 = distr1Typicality (=distr2Typicality)
+# p_l_t = literal listener probability to pick target 
+# p_l_d1 = literal listener probability to pick distractor1 
+# p_l_d2 = literal listener probability to pick distractor2 
 
