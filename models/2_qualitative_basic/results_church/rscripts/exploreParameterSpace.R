3bfea197797a7c5025a86504c7d659f6f3616cbc
thegricean <judithdegen@gmail.com>
2016-07-04 11:34:42 -0700

    added basic asymmetry qualitative model predictions

---
+++ models/2_qualitative_basic/results_church/rscripts/exploreParameterSpace.R
@@ -0,0 +1,146 @@
+theme_set(theme_bw(18))
+setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/1_basic_overinformativeness/results/")
+
+source("rscripts/helpers.r")
+r = read.table("parsed/results.txt",quote="", sep=",", header=T)
+head(r)
+nrow(r)
+names(r)
+summary(r)
+d=r
+
+# gather values
+r = r %>%
+  gather(Utterance,Probability,big:small_yellow)
+nrow(r)
+summary(r)
+r$speaker.opt = as.factor(as.character(r$speaker.opt))
+save(r,file="parsed/r.RData")
+
+##################
+# PLOTS OF UTTERANCE PROBABILITY AS A FUNCTION OF SPEAKER OPTIMALITY (COLOR), INCREASING COST OF SIZE (COLUMNS) AND INCREASING COST OF COLOR (ROWS) FOR FIXED VALUES OF OBJECT, COLOR FIDELITY, AND SIZE FIDELITY
+
+ggplot(r[r$object == "o1" & r$color_fidelity == .999 & r$size_fidelity == .999,],aes(x=Utterance,y=Probability,color=speaker.opt,group=speaker.opt)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(color_cost~size_cost) +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1)) +
+  ggtitle("columns: increasing size cost. rows: increasing color cost")
+ggsave("graphs/o1_cf.999_sf.999.pdf",width=25,height=20)
+
+ggplot(r[r$object == "o1" & r$color_fidelity == .999 & r$size_fidelity == .9,],aes(x=Utterance,y=Probability,color=speaker.opt,group=speaker.opt)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(color_cost~size_cost) +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1)) +
+  ggtitle("columns: increasing size cost. rows: increasing color cost")
+ggsave("graphs/o1_cf.999_sf.9.pdf",width=25,height=20)
+
+ggplot(r[r$object == "o1" & r$color_fidelity == .999 & r$size_fidelity == .8,],aes(x=Utterance,y=Probability,color=speaker.opt,group=speaker.opt)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(color_cost~size_cost) +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1)) +
+  ggtitle("columns: increasing size cost. rows: increasing color cost")
+ggsave("graphs/o1_cf.999_sf.8.pdf",width=25,height=20)
+
+ggplot(r[r$object == "o1" & r$color_fidelity == .8 & r$size_fidelity == .6,],aes(x=Utterance,y=Probability,color=speaker.opt,group=speaker.opt)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(color_cost~size_cost) +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1)) +
+  ggtitle("columns: increasing size cost. rows: increasing color cost")
+ggsave("graphs/o1_cf.8_sf.6.pdf",width=25,height=20)
+
+ggplot(r[r$object == "o1" & r$color_fidelity == .8 & r$size_fidelity == .7,],aes(x=Utterance,y=Probability,color=speaker.opt,group=speaker.opt)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(color_cost~size_cost) +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1)) +
+  ggtitle("columns: increasing size cost. rows: increasing color cost")
+ggsave("graphs/o1_cf.8_sf.7.pdf",width=25,height=20)
+
+ggplot(r[r$object == "o1" & r$color_fidelity == .8 & r$size_fidelity == .7,],aes(x=Utterance,y=Probability,color=speaker.opt,group=speaker.opt)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(color_cost~size_cost) +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1)) +
+  ggtitle("columns: increasing size cost. rows: increasing color cost")
+ggsave("graphs/o1_cf.8_sf.7.pdf",width=25,height=20)
+
+### TRYING TO HONE IN ON THE IMPORTANT REGIONS
+summary(d$big_red)
+d[d$object == "o1" & d$big_red > .75 & d$big < .25 & d$red < .05,c("color_fidelity","size_fidelity","big","red","big_red","speaker.opt","color_cost","size_cost")]
+
+summary(d[d$object == "o3" & d$small_yellow < .15 & d$small < .05 & d$yellow > .85 & d$color_cost == .1 & d$size_cost < .3 & d$color_fidelity > .75 & d$size_fidelity > .6 & d$color_fidelity > d$size_fidelity,c("color_fidelity","size_fidelity","small","yellow","small_yellow","speaker.opt","color_cost","size_cost")])
+
+d[d$object == "o3" & d$color_cost == .1 & d$size_cost == .1 & d$color_fidelity > .75 & d$size_fidelity > .6 & d$color_fidelity > d$size_fidelity & d$speaker.opt > 10 & d$yellow > .85,c("color_fidelity","size_fidelity","small","yellow","small_yellow","speaker.opt")]
+
+d[d$object == "o1" & d$color_cost == .1 & d$size_cost == .1 & d$color_fidelity == .999 & d$size_fidelity > .6 & d$color_fidelity > d$size_fidelity & d$speaker.opt == 18,c("color_fidelity","size_fidelity","big","red","big_red")]
+
+### PLOT JUST THE RANGES THAT ARE RELEVANT
+# COLOR COST .1
+# SIZE COST .1 AND .2
+# SPEAKER OPTIMALITY > 10
+# COLOR FIDELITY .85, .9, .999
+# SIZE FIDELITY .7, .75, .8
+
+ggplot(r[r$object == "o1" & r$color_fidelity > .6 & r$size_fidelity > .6  & r$color_cost == .1 & r$size_cost == .1 & (as.numeric(as.character(r$speaker.opt)) > 10 | as.numeric(as.character(r$speaker.opt)) == 5),],aes(x=Utterance,y=Probability,color=speaker.opt,group=speaker.opt)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(color_fidelity~size_fidelity) +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1)) +
+  ggtitle("columns: increasing size fidelity. rows: increasing color fidelity")
+ggsave("graphs/o1_cc.1_sc.1.pdf",width=20,height=15)
+
+ggplot(r[r$object == "o2" & r$color_fidelity > .6 & r$size_fidelity > .6  & r$color_cost == .1 & r$size_cost == .1 & (as.numeric(as.character(r$speaker.opt)) > 10 | as.numeric(as.character(r$speaker.opt)) == 5),],aes(x=Utterance,y=Probability,color=speaker.opt,group=speaker.opt)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(color_fidelity~size_fidelity) +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1)) +
+  ggtitle("columns: increasing size fidelity. rows: increasing color fidelity")
+ggsave("graphs/o2_cc.1_sc.1.pdf",width=20,height=15)
+
+ggplot(r[r$object == "o3" & r$color_fidelity > .6 & r$size_fidelity > .6  & r$color_cost == .1 & r$size_cost == .1 & (as.numeric(as.character(r$speaker.opt)) > 10 | as.numeric(as.character(r$speaker.opt)) == 5),],aes(x=Utterance,y=Probability,color=speaker.opt,group=speaker.opt)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(color_fidelity~size_fidelity) +
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1)) +
+  ggtitle("columns: increasing size fidelity. rows: increasing color fidelity")
+ggsave("graphs/o3_cc.1_sc.1.pdf",width=20,height=15)
+
+### PLOT MODEL PREDICTIONS WITH (EYE-BALLING) BEST PARAMETERS FOR ALL THREE OBJECTS ALONGSIDE EMPIRICAL DATA FROM GATT ET AL 2011 (BOTH ENGLISH AND DUTCH)
+
+t = droplevels(subset(r, color_fidelity == .999 & size_fidelity == .8 & color_cost == .1 & size_cost == .1 & speaker.opt == 17))
+t
+t$DataType = "model prediction"
+t$object = as.factor(ifelse(t$object == "o1", "big red", ifelse(t$object == "o2", "small red", "small yellow")))
+
+emp = data.frame(object = rep(rep(c("big red","small red","small yellow"),each=7),2),DataType=rep(c("empirical (Dutch)","empirical (English)"),each=21),Utterance=rep(levels(r$Utterance),6),Probability=c(.21,.003,.79,0,0,0,0,NA,NA,NA,NA,NA,NA,NA,0,0,0,.003,0,.9,.1,.17,.03,.8,0,0,0,0,NA,NA,NA,NA,NA,NA,NA,0,0,0,0,0,.92,.08))
+head(emp)
+
+toplot = merge(t,emp,all=T)
+toplot$DataType = as.factor(as.character(toplot$DataType))
+summary(toplot)
+
+ggplot(toplot, aes(x=Utterance,y=Probability,fill=DataType)) +
+  geom_bar(stat="identity",position="dodge") +
+  facet_wrap(~object) +
+  ggtitle(paste("c_fidelity ",unique(t$color_fidelity), ", s_fidelity ",unique(t$size_fidelity), ", c_cost ", unique(t$color_cost), ", s_cost ", unique(t$size_cost), ", sp_opt ", unique(t$speaker.opt),sep="")) +  
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1),plot.title = element_text(size=10)) 
+  
+ggsave("graphs/model.vs.empirical.pdf",height=6)
+ggsave("graphs/model.vs.empirical.jpg")
+
+
+# plot for jhu talk
+toplot = droplevels(subset(t, object != "small red" & DataType == "model prediction"))
+toplot$RedundantProperty = ifelse(toplot$object == "big red","color redundant","size redundant")
+toplot$UtteranceType = ifelse(toplot$Utterance %in% c("big_red","small_yellow"), "redundant","minimal")
+toplot$Correct = ifelse((toplot$object == "big red" & toplot$Utterance %in% c("big_red","big") | toplot$object == "small yellow" & toplot$Utterance %in% c("small_yellow","yellow")), "correct","incorrect")
+
+ggplot(toplot[toplot$Correct == "correct" & toplot$UtteranceType == "redundant",], aes(x=RedundantProperty,y=Probability)) +
+  geom_bar(stat="identity",fill="gray60",color="black") +
+  ylab("Probability of redundancy") +
+  scale_x_discrete(breaks=c("color redundant","size redundant"),labels=c("color\nredundant","size\nredundant"),name="")
+ggsave("graphs/color_size_asymm.pdf",height=3.5,width=3)
---
+++ models/2_qualitative_basic/results_wppl/rscripts/modelexploration.R
@@ -0,0 +1,117 @@
+setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/2_qualitative_basic/")
+
+typtype = "unlogged" 
+# typtype = "logged"
+searchtype = "wide"
+# searchtype = "narrow"
+
+######### PLOT LISTENER #########
+
+dlistener = read.table(paste("results_wppl/data/listener_",typtype,"_",searchtype,".csv",sep=""),sep=",")
+colnames(dlistener) = c("alpha","lengthWeight","typicality_color","typicality_size","cost_color","cost_size","utterance","object","probability")
+head(dlistener)
+summary(dlistener)
+
+dlistener = unique(dlistener[,c("typicality_color","typicality_size","utterance","object","probability")])
+nrow(dlistener)
+
+# relevant for size sufficient condition:
+dlss = dlistener[dlistener$utterance %in% c("blue_thumbtack","size_thumbtack","size_blue_thumbtack"),]
+dlss$Object = as.factor(ifelse(dlss$object == "size,blue,thumbtack","target",ifelse(dlss$object == "othersize,blue,thumbtack","color_competitor","distractor")))
+dlss$Utterance = as.factor(ifelse(dlss$utterance == "blue_thumbtack","color",ifelse(dlss$utterance == "size_thumbtack","size","size_color")))
+ggplot(dlss, aes(x=Utterance,y=probability,fill=Object)) +
+  geom_bar(stat="identity",position="dodge") +
+  facet_grid(typicality_color~typicality_size) +
+  theme(axis.text.x = element_text(angle=45,hjust=1,vjust=1)) +
+  ggtitle("Size fidelity increases left to right; color increases top to bottom")
+ggsave(paste("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/2_qualitative_basic/results_wppl/graphs/listenerdist_sizesufficient_",typtype,"_",searchtype,".pdf",sep=""),height=12,width=15)
+
+# relevant for color sufficient condition:
+dlcs = dlistener[dlistener$utterance %in% c("red_thumbtack","othersize_thumbtack","othersize_red_thumbtack"),]
+dlcs$Object = as.factor(ifelse(dlcs$object == "size,blue,thumbtack","distractor",ifelse(dlcs$object == "othersize,blue,thumbtack","size_competitor","target")))
+dlcs$Utterance = as.factor(ifelse(dlcs$utterance == "red_thumbtack","color",ifelse(dlcs$utterance == "othersize_thumbtack","size","size_color")))
+ggplot(dlcs, aes(x=Utterance,y=probability,fill=Object)) +
+  geom_bar(stat="identity",position="dodge") +
+  facet_grid(typicality_color~typicality_size) +
+  theme(axis.text.x = element_text(angle=45,hjust=1,vjust=1)) +
+  ggtitle("Size fidelity increases left to right; color increases top to bottom")
+ggsave(paste("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/2_qualitative_basic/results_wppl/graphs/listenerdist_colorsufficient_",typtype,"_",searchtype,".pdf",sep=""),height=12,width=15)
+
+######### PLOT SPEAKER #########
+
+dspeaker = read.table(paste("results_wppl/data/speaker_",typtype,"_",searchtype,".csv",sep=""),sep=",")
+colnames(dspeaker) = c("alpha","lengthWeight","typicality_color","typicality_size","cost_color","cost_size","sufficientdimension","utterance","probability")
+head(dspeaker)
+summary(dspeaker)
+nrow(dspeaker)
+
+d = dspeaker
+d$typicality_size = as.factor(as.character(d$typicality_size))
+ggplot(d[d$cost_color == 1 & d$cost_size == 1 & d$sufficientdimension == "size_sufficient" & d$lengthWeight == 1,], aes(x=typicality_color,y=probability,color=typicality_size,group=typicality_size)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(alpha~utterance)
+ggsave(paste("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/2_qualitative_basic/results_wppl/graphs/modelexploration-",typtype,"_",searchtype,".pdf",sep=""),height=10,width=15)
+
+spreadspeaker = dspeaker %>%
+  spread(utterance,probability)
+
+# cases that match size sufficient condition from gatt et al 2011
+ss = spreadspeaker[spreadspeaker$sufficientdimension == "size_sufficient" & spreadspeaker$size_blue_thumbtack > .78 & spreadspeaker$size_blue_thumbtack < .81 & spreadspeaker$size_thumbtack > .16 & spreadspeaker$size_thumbtack < .22 & spreadspeaker$blue_thumbtack < .04,]
+nrow(ss)
+row.names(ss) = paste(ss$alpha,ss$lengthWeight,ss$typicality_color,ss$typicality_size,ss$cost_color,ss$cost_size)
+
+cs = spreadspeaker[spreadspeaker$sufficientdimension == "color_sufficient" & spreadspeaker$othersize_red_thumbtack > .07 & spreadspeaker$othersize_red_thumbtack < .11 & spreadspeaker$othersize_thumbtack < .01 & spreadspeaker$red_thumbtack < .925 & spreadspeaker$red_thumbtack > .89,]
+nrow(cs)
+row.names(cs) = paste(cs$alpha,cs$lengthWeight,cs$typicality_color,cs$typicality_size,cs$cost_color,cs$cost_size)
+
+intersect(row.names(ss),row.names(cs))
+# logged, wide search: the top 5 cases are
+# [1] "15 2 0.999 0.9 0 0"   "15 2 0.999 0.9 0 0.5" "15 2 0.999 0.9 0 1"  
+# [4] "15 2 0.999 0.9 0 1.5" "15 2 0.999 0.9 0 2"  
+# logged, narrow search: the top 10 cases are
+#  [1] "14 2.5 0.999 0.9 0 0"     "14 2.5 0.999 0.9 0 0.1"   "14 2.5 0.999 0.9 0 0.5"  
+# [4] "14 2.5 0.999 0.9 0 1"     "14 2.5 0.999 0.9 0 2"     "17 2.5 0.999 0.9 0.1 0"  
+# [7] "17 2.5 0.999 0.9 0.1 0.1" "17 2.5 0.999 0.9 0.1 0.5" "17 2.5 0.999 0.9 0.1 1"  
+# [10] "17 2.5 0.999 0.9 0.1 2"
+
+# unlogged, narrow search:
+# [1] "28 0 0.999 0.8 0 0"     "28 0 0.999 0.8 0 0.1"   "28 0 0.999 0.8 0 0.2"  
+# [4] "28 0 0.999 0.8 0.1 0"   "28 0 0.999 0.8 0.1 0.1" "28 0 0.999 0.8 0.1 0.2"
+# [7] "28 0 0.999 0.8 0.2 0"   "28 0 0.999 0.8 0.2 0.1" "28 0 0.999 0.8 0.2 0.2"
+
+# plot for paper
+dp = dspeaker[dspeaker$lengthWeight == 0 & dspeaker$cost_color == 1 & dspeaker$cost_size == 1 & dspeaker$sufficientdimension == "size_sufficient" & dspeaker$utterance %in% c("blue_thumbtack","size_thumbtack","size_blue_thumbtack"),]
+dp$utterance = as.factor(ifelse(dp$utterance == "blue_thumbtack","insufficient",ifelse(dp$utterance == "size_thumbtack","sufficient","redundant")))
+dp = dp %>%
+  mutate(typicality_sufficient = typicality_size, typicality_insufficient = typicality_color) %>%
+  select(alpha,typicality_sufficient,typicality_insufficient, utterance,probability)
+nrow(dp)
+head(dp)
+
+dp$typicality_insufficient = as.factor(as.character(dp$typicality_insufficient))
+dp$Utterance = factor(x=as.character(dp$utterance),levels=c("insufficient","sufficient","redundant"))
+ggplot(dp[dp$typicality_sufficient != "0.95" & dp$typicality_insufficient != .95,], aes(x=typicality_sufficient,y=probability,color=typicality_insufficient,group=typicality_insufficient)) +
+  geom_point(size=1) +
+  geom_line(size=1.5) +
+  geom_hline(yintercept=.5,color="gray20", linetype="dashed") +
+  ylab("Probability of utterance") +
+  xlab("Fidelity of sufficient utterance") +
+  # scale_colour_brewer(palette = "PiYG",name="Fidelity of\nsufficient\nutterance") +
+  scale_colour_brewer(palette = "RdYlBu",name="Fidelity of\nsufficient\nutterance") +
+  facet_grid(alpha~Utterance) 
+ggsave(paste("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/2_qualitative_basic/results_wppl/graphs/modelexploration-fullfidelityeffect-",typtype,"-",searchtype,".pdf",sep=""),height=11,width=9)
+ggsave(paste("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/writing/2016/theory/pics/modelexploration-fullfidelityeffect-",typtype,"-",searchtype,".pdf",sep=""),height=11,width=9)
+
+dp$typicality_insufficient = as.factor(as.character(dp$typicality_insufficient))
+ggplot(dp[dp$alpha == 30 & dp$typicality_sufficient != "0.95" & dp$typicality_insufficient != .95,], aes(x=typicality_sufficient,y=probability,color=typicality_insufficient,group=typicality_insufficient)) +
+  geom_point(size=1) +
+  geom_line(size=1.5) +
+  geom_hline(yintercept=.5,color="gray20", linetype="dashed") +
+  ylab("Probability of utterance") +
+  xlab("Fidelity of sufficient utterance") +
+  # scale_colour_brewer(palette = "PiYG",name="Fidelity of\nsufficient\nutterance") +
+  scale_colour_brewer(palette = "RdYlBu",name="Fidelity of\ninsufficient\nutterance") +
+  facet_wrap(~Utterance) 
+ggsave(paste("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/2_qualitative_basic/results_wppl/graphs/modelexploration-fidelityeffect-",typtype,"-",searchtype,".pdf",sep=""),height=3.5,width=10)
+ggsave(paste("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/writing/2016/theory/pics/modelexploration-fidelityeffect-",typtype,"-",searchtype,".pdf",sep=""),height=4,width=11)
