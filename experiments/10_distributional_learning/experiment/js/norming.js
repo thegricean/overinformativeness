5251562be4edc7af9b5eef16bffe46754ee5fee3
Elisa Kreiss <elisakreiss@yahoo.de>
2016-10-12 10:05:29 -0700

    production calibration

--- experiments/10_distributional_learning/experiment/js/norming.js
+++ experiments/10_distributional_learning/experiment/js/norming.js
@@ -437,7 +437,7 @@ function make_slides(f) {
       exp.sliderPost = {};
 
     	// How typical is this color for this object?
-      var contextsentence = "Please, answer intuitively: How typical is this color for this object?";
+      var contextsentence = "How typical is this color for this object on Daxy's planet?";
     	var objimagehtml = '<img src="pictures/'+stim.item+'_'+stim.color+'.png" style="height:190px;">';
     	$("#contextsentence").html(contextsentence);
     	$("#objectimage").html(objimagehtml);
@@ -761,7 +761,7 @@ function init() {
       screenUW: exp.width
     };
   //blocks of the experiment:
-  exp.structure=["i0", "intro", /*"exposure",*/ "intro2", "production", "intro3", "objecttrial", 'subj_info', 'thanks'];
+  exp.structure=["i0", "intro", "exposure", "intro2", "production", "intro3", "objecttrial", 'subj_info', 'thanks'];
   
   exp.data_trials = [];
   //make corresponding slides:
