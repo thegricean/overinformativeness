dd3cc9cd631076c66326334038f8a1d3049443da
Elisa Kreiss <elisakreiss@yahoo.de>
2016-10-12 11:55:52 -0700

    add distributional exp

---
+++ experiments/13_distribution/experiment/createPreloadList.py
@@ -0,0 +1,4 @@
+import os
+
+
+print("\",\"pictures/".join(os.listdir("/Users/elisakreiss/Documents/Stanford/study/overinformativeness/experiments/10_distributional_learning/experiment/pictures/")))
