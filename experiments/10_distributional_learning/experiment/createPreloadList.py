a51d167e25054ee177351fbdc88a4a5e894d192f
Elisa Kreiss <elisakreiss@yahoo.de>
2016-10-18 17:23:00 -0700

    exp 19 update

--- experiments/10_distributional_learning/experiment/createPreloadList.py
+++ experiments/10_distributional_learning/experiment/createPreloadList.py
@@ -1,4 +1,4 @@
 import os
 
 
-print("\",\"pictures/".join(os.listdir("/Users/elisakreiss/Documents/Stanford/study/overinformativeness/experiments/10_distributional_learning/experiment/pictures/")))
+print("\",\"pictures/".join(os.listdir("/Users/elisakreiss/Documents/stanford/study/overinformativeness/experiments/19_18_enhanced/experiment/pictures")))
---
+++ experiments/19_18_enhanced/experiment/createPreloadList.py
@@ -0,0 +1,4 @@
+import os
+
+
+print("\",\"".join(os.listdir("/Users/elisakreiss/Documents/stanford/study/overinformativeness/experiments/19_18_enhanced/Greebles-2-0-symmetric/Images")))
