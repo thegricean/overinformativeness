6d37f4f2a8949582668b1c24d71ceb5b278ab29d
Elisa Kreiss <elisakreiss@yahoo.de>
2016-10-24 19:10:44 -0700

    modelexploration update

--- models/Elisa_colortypicality/results_wppl/rscripts/modelexploration.R
+++ models/Elisa_colortypicality/results_wppl/rscripts/modelexploration.R
@@ -1,24 +1,24 @@
-setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/Elisa_colortypicality/")
-
+# setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/Elisa_colortypicality/")
+setwd("/Users/elisakreiss/Documents/stanford/study/overinformativeness/models/Elisa_colortypicality/")
 
 ######### PLOT LISTENER #########
 
 dlistener = read.table(paste("results_wppl/data/listener_exploration.csv",sep=""),sep=",")
 
 # fix headers:
-colnames(dlistener) = c("alpha","lengthWeight","","","cost_color","cost_type","utterance","object","probability") 
+colnames(dlistener) = c("alpha","lengthWeight","u_banana_o_yellow_banana","u_banana_o_blue_banana","u_cup_o_yellow_cup","u_cup_o_blue_cup","u_yellow_banana_o_yellow_banana","u_blue_banana_o_blue_banana","u_yellow_cup_o_yellow_cup","u_blue_cup_o_blue_cup","u_apple_o_yellow_apple","u_apple_o_blue_apple","u_yellow_apple_o_yellow_apple","u_blue_apple_o_blue_apple","cost_color","cost_type","utterance","object","probability")
 head(dlistener)
 summary(dlistener)
 
-bananatestplot = dlistener[dlistener$V17 %in% c("banana","yellow_banana"),]
+bananatestplot = dlistener[dlistener$utterance %in% c("banana","yellow_banana"),]
 summary(bananatestplot)
 nrow(bananatestplot)
 
-bananatestplot = unique(bananatestplot[,c("V3","V4","V6","V7","V8","V10","V17","V18","V19")])
+bananatestplot = unique(bananatestplot[,c("u_banana_o_yellow_banana","u_banana_o_blue_banana","u_cup_o_yellow_cup","u_cup_o_blue_cup","u_yellow_banana_o_yellow_banana","u_blue_banana_o_blue_banana","u_yellow_cup_o_yellow_cup","u_blue_cup_o_blue_cup","u_apple_o_yellow_apple","u_apple_o_blue_apple","u_yellow_apple_o_yellow_apple","u_blue_apple_o_blue_apple","cost_color","cost_type","utterance","object","probability")])
 head(bananatestplot)
 
-ggplot(bananatestplot,aes(x=V18,y=V19,color=as.factor(V3),group=interaction(V3,V7,V8),shape=as.factor(V8),linetype=as.factor(V7))) +
+ggplot(bananatestplot,aes(x=object,y=probability,color=as.factor(u_banana_o_yellow_banana),group=interaction(u_banana_o_yellow_banana,u_yellow_banana_o_yellow_banana,u_blue_banana_o_blue_banana),shape=as.factor(u_blue_banana_o_blue_banana),linetype=as.factor(u_yellow_banana_o_yellow_banana))) +
   geom_point() +
   geom_line() +
-  facet_grid(V17~V4) +
+  facet_grid(utterance~u_banana_o_blue_banana) +
   theme(axis.text.x = element_text(angle=45,hjust=1,vjust=1))
