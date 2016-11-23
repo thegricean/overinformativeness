80f1ff3a050691d4926794df86e5ebf87b6647b3
thegricean <judithdegen@gmail.com>
2016-11-23 12:02:39 -0800

    added new results

--- experiments/25_object_norming/results/rscripts/norming.r
+++ experiments/25_object_norming/results/rscripts/norming.r
@@ -88,7 +88,13 @@ ggplot(d, aes(x=response,fill=Color)) +
   facet_wrap(~Item,nrow=2,scales="free")
 ggsave("graphs/typicalities_histograms.pdf",height=5,width=10)
 
-agr = d %>% 
+nocups = d[grep("cup",d$object,invert=T),]
+nocups = nocups[grep("purple",nocups$object,invert=T),]
+nocups = nocups[grep("pepper_green",nocups$object,invert=T),]
+nocups = nocups[grep("cup",nocups$utterance,invert=T),]
+nocups = droplevels(nocups)
+
+agr = nocups %>% 
   group_by(Item,Color,utterance) %>%
   summarise(MeanTypicality = mean(response), ci.low=ci.low(response),ci.high=ci.high(response))
 agr = as.data.frame(agr)
@@ -96,12 +102,15 @@ agr$YMin = agr$MeanTypicality - agr$ci.low
 agr$YMax = agr$MeanTypicality + agr$ci.high
 
 agr$Combo = paste(agr$Color,agr$Item)
-agr$OrdCombo = factor(agr$Combo, levels=agr[order(agr$MeanTypicality), "Combo"])
+#agr$OrdCombo = factor(agr$Combo, levels=agr[order(agr$MeanTypicality), "Combo"])
+#agr$OrdCombo = factor(x=as.character(agr$Combo), levels=agr[order(agr$MeanTypicality,decreasing=T), "Combo"])
+#agr = agr[order(agr[,c("MeanTypicality")],decreasing=T),]
 
-ggplot(agr, aes(x=OrdCombo,y=MeanTypicality)) +
+ggplot(agr, aes(x=Combo,y=MeanTypicality,color=Color)) +
   geom_point() +
   geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
   facet_wrap(~utterance,scales="free_x",nrow=4) +
+  scale_color_manual(values=as.character(agr$Color)) +
   theme(axis.text.x = element_text(angle=45,size=5,vjust=1,hjust=1))
 ggsave("graphs/typicalities.png",height=9, width=15)
 
