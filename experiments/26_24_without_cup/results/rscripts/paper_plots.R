9751dc32397c6abf51449af99bb14553c52c32bf
Elisa Kreiss <elisakreiss@Elisas-MacBook-Pro.local>
2017-01-10 17:18:12 +0100

    clean plots

--- experiments/26_24_without_cup/results/rscripts/paper_plots.R
+++ experiments/26_24_without_cup/results/rscripts/paper_plots.R
@@ -6,7 +6,7 @@ library(tidyr)
 
 theme_set(theme_bw(18))
 setwd("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/experiments/26_24_without_cup/results")
-setwd("/Users/elisakreiss/Documents/Stanford/Study/overinformativeness/experiments/26_24_without_cup/results")
+setwd("/Users/elisakreiss/Documents/Stanford/overinformativeness/experiments/26_24_without_cup/results")
 source("rscripts/helpers.r")
 
 d = read.table(file="data/results.csv",sep="\t", header=T, quote="")
@@ -15,33 +15,15 @@ head(d)
 unique(d$listenerMessages)
 d[d$listenerMessages != "",c("listenerMessages","speakerMessages")]
 summary(d)
-d = d[!d$speakerMessages == "",]
+# exclude NA referential experssions 
+d = d[!(is.na(d$refExp)),]
+#d = d[!d$speakerMessages == "",]
 head(d)
 nrow(d)
-
-# look at turker comments
-comments = read.table(file="data/overinf.csv",sep=",", header=T, quote="")
-
-unique(comments$comments)
-
-ggplot(comments, aes(ratePartner)) +
-  stat_count()
-
-ggplot(comments, aes(thinksHuman)) +
-  stat_count()
-
-ggplot(comments, aes(nativeEnglish)) +
-  stat_count()
-
-ggplot(comments, aes(totalLength)) +
-  geom_histogram()
 
 # first figure out how often target was chosen -- exclude trials where it wasn't?
 table(d$context,d$targetStatusClickedObj)
 
-# exclude NA referential experssions 
-d = d[!(is.na(d$refExp)),]
-nrow(d)
 
 # exclude trials with distractor choices
 d = droplevels(d[d$targetStatusClickedObj == "target",])
@@ -57,8 +39,9 @@ length(levels(d$gameid))
 table(d$context,d$typeMentioned)
 table(d$context,d$colorMentioned)
 
+# get meantypicalities from previous study
 typ = read.csv(file="/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/experiments/25_object_norming/results/data/meantypicalities.csv")
-typ = read.csv(file="/Users/elisakreiss/Documents/Stanford/Study/overinformativeness/experiments/25_object_norming/results/data/meantypicalities.csv")
+typ = read.csv(file="/Users/elisakreiss/Documents/Stanford/overinformativeness/experiments/25_object_norming/results/data/meantypicalities.csv")
 typ = typ[as.character(typ$Item) == as.character(typ$utterance),]
 row.names(typ) = paste(typ$Color,typ$Item)
 head(typ)
@@ -96,12 +79,7 @@ table(production[production$UtteranceType == "OTHER",]$gameid)
 production$Color = ifelse(production$UtteranceType == "color",1,0)
 production$ColorAndType = ifelse(production$UtteranceType == "color_and_type",1,0)
 production$Type = ifelse(production$UtteranceType == "type",1,0)
-production$ColorAndCat = ifelse(production$UtteranceType == "color_and_cat",1,0)
-production$Cat = ifelse(production$UtteranceType == "cat",1,0)
-production$Neg = ifelse(production$UtteranceType == "negation",1,0)
-production$Description = ifelse(production$UtteranceType == "description",1,0)
-production$ColorModifier = ifelse(production$UtteranceType == "color_modifier",1,0)
-production$Other = ifelse(production$UtteranceType == "OTHER",1,0)
+production$Other = ifelse(production$UtteranceType == "OTHER" | production$UtteranceType == "color_and_cat" | production$UtteranceType == "cat" | production$UtteranceType == "negation" | production$UtteranceType == "description" | production$UtteranceType == "color_modifier",1,0)
 production$Item = production$clickedType
 production$Half = ifelse(production$roundNum < 21,1,2)
 
@@ -149,7 +127,7 @@ ggsave("graphs/mentioned_features_by_context.png",width=8,height=3.5)
 
 # plot utterance choice proportions with error bars
 agr = production %>%
-  select(Color,Type,ColorAndType,ColorAndCat,Cat,Neg,Description,ColorModifier,Other,context) %>%
+  select(Color,Type,ColorAndType,Other,context) %>%
   gather(Utterance,Mentioned,-context) %>%
   group_by(Utterance,context) %>%
   summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned))
@@ -157,18 +135,18 @@ agr = as.data.frame(agr)
 agr$YMin = agr$Probability - agr$ci.low
 agr$YMax = agr$Probability + agr$ci.high
 
-  # change order of Utterance column
+# change order of Utterance column
 agr$Utterance <- as.character(agr$Utterance)
-agr$Utterance <- factor(agr$Utterance, levels=c("Type", "Color", "Cat", "ColorAndType", "ColorAndCat", "ColorModifier", "Description", "Neg", "Other"))
+agr$Utterance <- factor(agr$Utterance, levels=c("Type", "Color", "ColorAndType", "Other"))
 
-  # change context names to have nicer facet labels 
+# change context names to have nicer facet labels 
 levels(agr$context) = c("informative","informative\nwith color competitor", "overinformative", "overinformative\nwith color competitor")
 
 ggplot(agr, aes(x=Utterance,y=Probability)) +
   geom_bar(stat="identity",width=.2,fill="orange",colour="orange") +
   geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.15,colour="grey") +
   facet_wrap(~context) +
-  scale_x_discrete(labels=c("Only Type", "Only Color", "Only Category", "Color + Type", "Color + Category", "Includes Color Modifier", "Includes Description", "Includes Negation", "Other")) +
+  scale_x_discrete(labels=c("Only Type", "Only Color", "Color + Type", "Other")) +
   theme(axis.title=element_text(size=14,colour="#757575")) +
   theme(axis.text.x=element_text(angle=45,hjust=1,vjust=1,size=11,colour="#757575")) +
   theme(axis.text.y=element_text(size=10,colour="#757575")) +
@@ -176,12 +154,12 @@ ggplot(agr, aes(x=Utterance,y=Probability)) +
   theme(strip.text.x=element_text(size=12,colour="#757575")) +
   theme(strip.background=element_rect(colour="#939393",fill="white")) +
   theme(panel.background=element_rect(colour="#939393"))
-ggsave("graphs/mentioned_features_by_context_other.png",width=7,height=7)
+ggsave("graphs/paper/mentioned_features_by_context_other.png",width=7,height=7)
 
 
 # plot utterance choice proportions by typicality
 agr = production %>%
-  select(Color,Type,ColorAndType,Neg,ColorAndCat,Description,ColorModifier,Other,binaryTypicality,context) %>%
+  select(Color,Type,ColorAndType,Other,binaryTypicality,context) %>%
   gather(Utterance,Mentioned,-context,-binaryTypicality) %>%
   group_by(Utterance,context,binaryTypicality) %>%
   summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned))
@@ -189,11 +167,11 @@ agr = as.data.frame(agr)
 agr$YMin = agr$Probability - agr$ci.low
 agr$YMax = agr$Probability + agr$ci.high
 
-  # change order of Utterance column
+# change order of Utterance column
 agr$Utterance <- as.character(agr$Utterance)
-agr$Utterance <- factor(agr$Utterance, levels=c("Type", "Color", "Cat", "ColorAndType", "ColorAndCat", "ColorModifier", "Description", "Neg", "Other"))
+agr$Utterance <- factor(agr$Utterance, levels=c("Type", "Color", "ColorAndType", "Other"))
 
-  # change context names to have nicer facet labels 
+# change context names to have nicer facet labels 
 levels(agr$context) = c("informative","informative\nwith color competitor", "overinformative", "overinformative\nwith color competitor")
 
 ggplot(agr, aes(x=binaryTypicality,y=Probability,color=Utterance,group=Utterance)) +
@@ -202,8 +180,8 @@ ggplot(agr, aes(x=binaryTypicality,y=Probability,color=Utterance,group=Utterance
   #geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
   facet_wrap(~context) +
   scale_color_discrete(name="Utterance",
-                      breaks=c("Type", "Color", "Cat", "ColorAndType", "ColorAndCat", "ColorModifier", "Description", "Neg", "Other"),
-                      labels=c("Only Type", "Only Color", "Only Category", "Color + Type", "Color + Category", "Includes Color Modifier", "Includes Description", "Includes Negation", "Other")) +
+                       breaks=c("Type", "Color", "ColorAndType", "Other"),
+                       labels=c("Only Type", "Only Color", "Color + Type", "Other")) +
   theme(axis.title=element_text(size=14,colour="#757575")) +
   theme(axis.text.x=element_text(size=11,colour="#757575")) +
   theme(axis.text.y=element_text(size=10,colour="#757575")) +
@@ -213,11 +191,13 @@ ggplot(agr, aes(x=binaryTypicality,y=Probability,color=Utterance,group=Utterance
   theme(legend.text=element_text(size=11,colour="#757575")) +
   theme(strip.background=element_rect(colour="#939393",fill="white")) +
   theme(panel.background=element_rect(colour="#939393"))
-ggsave("graphs/utterance_by_binarytyp.png",width=10,height=6.5)
+ggsave("graphs/paper/utterance_by_binarytyp.png",width=10,height=6.5)
+
+
 
 # plot utterance choice proportions by typicality
 agr = production %>%
-  select(Color,Type,ColorAndType,Neg,ColorAndCat,Description,ColorModifier,Other,NormedTypicality,context) %>%
+  select(Color,Type,ColorAndType,Other,NormedTypicality,context) %>%
   gather(Utterance,Mentioned,-context,-NormedTypicality) %>%
   group_by(Utterance,context,NormedTypicality) %>%
   summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned))
@@ -225,11 +205,11 @@ agr = as.data.frame(agr)
 agr$YMin = agr$Probability - agr$ci.low
 agr$YMax = agr$Probability + agr$ci.high
 
-  # change order of Utterance column
+# change order of Utterance column
 agr$Utterance <- as.character(agr$Utterance)
-agr$Utterance <- factor(agr$Utterance, levels=c("Type", "Color", "Cat", "ColorAndType", "ColorAndCat", "ColorModifier", "Description", "Neg", "Other"))
+agr$Utterance <- factor(agr$Utterance, levels=c("Type", "Color", "ColorAndType", "Other"))
 
-  # change context names to have nicer facet labels 
+# change context names to have nicer facet labels 
 levels(agr$context) = c("informative","informative\nwith color competitor", "overinformative", "overinformative\nwith color competitor")
 
 ggplot(agr, aes(x=NormedTypicality,y=Probability,color=Utterance)) +
@@ -238,8 +218,8 @@ ggplot(agr, aes(x=NormedTypicality,y=Probability,color=Utterance)) +
   #geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
   facet_wrap(~context) +
   scale_color_discrete(name="Utterance",
-                       breaks=c("Type", "Color", "Cat", "ColorAndType", "ColorAndCat", "ColorModifier", "Description", "Neg", "Other"),
-                       labels=c("Only Type", "Only Color", "Only Category", "Color + Type", "Color + Category", "Includes Color Modifier", "Includes Description", "Includes Negation", "Other")) +
+                       breaks=c("Type", "Color", "ColorAndType", "Other"),
+                       labels=c("Only Type", "Only Color", "Color + Type", "Other")) +
   theme(axis.title=element_text(size=14,colour="#757575")) +
   theme(axis.text.x=element_text(size=10,colour="#757575")) +
   theme(axis.text.y=element_text(size=10,colour="#757575")) +
@@ -249,121 +229,5 @@ ggplot(agr, aes(x=NormedTypicality,y=Probability,color=Utterance)) +
   theme(legend.text=element_text(size=11,colour="#757575")) +
   theme(strip.background=element_rect(colour="#939393",fill="white")) +
   theme(panel.background=element_rect(colour="#939393"))
-ggsave("graphs/utterance_by_conttyp.png",width=12,height=9)
+ggsave("graphs/paper/utterance_by_conttyp.png",width=12,height=9)
 
-# plot utterance choice proportions by typicality
-production$UmbrellaOther = ifelse(production$Color == 1 | production$Type == 1 | production$ColorAndType == 1, 0, 1)
-agr = production %>%
-  select(Color,Type,ColorAndType,UmbrellaOther,NormedTypicality,context) %>%
-  gather(Utterance,Mentioned,-context,-NormedTypicality) %>%
-  group_by(Utterance,context,NormedTypicality) %>%
-  summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned))
-agr = as.data.frame(agr)
-agr$YMin = agr$Probability - agr$ci.low
-agr$YMax = agr$Probability + agr$ci.high
-agr$Condition = factor(x=as.character(agr$context),levels=c("overinformative","overinformative-cc","informative","informative-cc"))
-agr$Utterance = as.character(agr$Utterance)
-agr[agr$Utterance == "UmbrellaOther",]$Utterance = "Other"
-agr$Utterance = factor(agr$Utterance, levels=c("Color","Type","ColorAndType","Other"))
-
-ggplot(agr, aes(x=NormedTypicality,y=Probability,color=Utterance)) +
-  geom_point(size=2) +
-  geom_smooth(method="lm") +
-  xlab("Typicality") +
-  ylab("Empirical utterance proportion") +
-  #geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
-  facet_wrap(~Condition)
-ggsave("../../../talks/2017/cuny/utterances.png",width=10,height=7)
-
-agr = production %>%
-  select(Color,Type,ColorAndType,UmbrellaOther,NormedTypicality,context,nameClickedObj) %>%
-  gather(Utterance,Mentioned,-context,-NormedTypicality,-nameClickedObj) %>%
-  group_by(Utterance,context,NormedTypicality,nameClickedObj) %>%
-  summarise(Probability=mean(Mentioned))
-agr = as.data.frame(agr)
-agr$Condition = factor(x=as.character(agr$context),levels=c("overinformative","overinformative-cc","informative","informative-cc"))
-agr$Utterance = as.character(agr$Utterance)
-agr[agr$Utterance == "UmbrellaOther",]$Utterance = "Other"
-agr$Utterance = factor(agr$Utterance, levels=c("Color","Type","ColorAndType","Other"))
-
-ggplot(agr, aes(x=NormedTypicality,y=Probability,color=Utterance)) +
-#  geom_point(size=2) +
-  geom_text(aes(label=nameClickedObj),size=2) +
-  # geom_smooth(method="lm") +
-  xlab("Typicality") +
-  ylab("Empirical utterance proportion") +
-  #geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
-  facet_wrap(~Condition)
-ggsave("../../../talks/2017/cuny/utterances_text.png",width=10,height=7)
-
-# by subject
-agr = production %>%
-  group_by(binaryContext,binaryTypicality,gameid) %>%
-  summarise(PropColorMentioned=mean(ColorAndType),ci.low=ci.low(ColorAndType),ci.high=ci.high(ColorAndType))
-agr = as.data.frame(agr)
-agr$YMin = agr$PropColorMentioned - agr$ci.low
-agr$YMax = agr$PropColorMentioned + agr$ci.high
-
-ggplot(agr, aes(x=binaryTypicality,y=PropColorMentioned,color=binaryContext)) +
-  geom_point() +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
-  facet_wrap(~gameid)
-ggsave("graphs/distribution_effect_production_bysubject.png",width=20,height=30)
-
-
-# plot utterance choice proportions by typicality
-agr = production %>%
-  select(Color,Type,ColorAndType,Neg,ColorAndCat,Description,ColorModifier,Other,binaryTypicality,context,Half) %>%
-  gather(Utterance,Mentioned,-context,-binaryTypicality,-Half) %>%
-  group_by(Utterance,context,binaryTypicality,Half) %>%
-  summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned))
-agr = as.data.frame(agr)
-agr$YMin = agr$Probability - agr$ci.low
-agr$YMax = agr$Probability + agr$ci.high
-
-ggplot(agr, aes(x=binaryTypicality,y=Probability,color=Utterance,group=Utterance)) +
-  geom_point() +
-  geom_line() +
-  #geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
-  facet_grid(Half~context)
-ggsave("graphs/utterance_by_binarytyp_byhalf.png",width=10,height=6.5)
-
-
-# write unique conditions for bda
-p_no_other = droplevels(production[production$UttforBDA != "other",])
-nrow(p_no_other)
-
-p_no_other$DistractorCombo = as.factor(ifelse(as.character(p_no_other$Dist1) < as.character(p_no_other$Dist2), paste(p_no_other$Dist1, p_no_other$Dist2), paste(p_no_other$Dist2, p_no_other$Dist1)))
-
-nrow(unique(p_no_other[,c("nameClickedObj","DistractorCombo")]))
-p_no_other$BDADist1 = sapply(strsplit(as.character(p_no_other$DistractorCombo)," "), "[", 1)
-p_no_other$BDADist2 = sapply(strsplit(as.character(p_no_other$DistractorCombo)," "), "[", 2)
-p_no_other$BDADist1Color = sapply(strsplit(as.character(p_no_other$BDADist1),"_"), "[", 2)
-p_no_other$BDADist1Type = sapply(strsplit(as.character(p_no_other$BDADist1),"_"), "[", 1)
-p_no_other$BDADist2Color = sapply(strsplit(as.character(p_no_other$BDADist2),"_"), "[", 2)
-p_no_other$BDADist2Type = sapply(strsplit(as.character(p_no_other$BDADist2),"_"), "[", 1)
-
-
-write.table(unique(p_no_other[,c("context","clickedColor","clickedType","BDADist1Color","BDADist1Type","BDADist2Color","BDADist2Type")]),file="/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/Elisa_colortypicality/bdaInput/unique_conditions.csv",sep=",",col.names=F,row.names=F,quote=F)
-
-# write data for bda
-write.table(p_no_other[,c("context","clickedColor","clickedType","BDADist1Color","BDADist1Type","BDADist2Color","BDADist2Type","UttforBDA")],file="/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/Elisa_colortypicality/bdaInput/bda_data.csv",sep=",",col.names=F,row.names=F,quote=F)
-
-############
-# Analysis #
-############
-
-# Exclude all "other" utterances
-an = droplevels(production[production$UttforBDA != "other",])
-nrow(an)
-
-centered = cbind(an,myCenter(an[,c("NormedTypicality","Informative","CC")]))
-centered$ColorOrType = centered$ColorAndType | centered$Color
-
-m = glmer(ColorOrType ~ cNormedTypicality + cInformative + cCC + cNormedTypicality : cInformative + cNormedTypicality:cCC + (1|gameid) + (1|Item), data = centered, family="binomial")
-summary(m)
-ranef(m)
-
-m = glmer(ColorOrType ~ cNormedTypicality + cInformative + cCC + (1|gameid) + (1|Item), data = centered, family="binomial")
-summary(m)
-ranef(m)
