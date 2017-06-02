524cd1ed3ab9ffab7aae8b2640ad715e142baaeb
Elisa Kreiss <elisakreiss@yahoo.de>
2017-06-02 17:09:32 +0200

    improved app design

--- experiments/elisa_paper_relevant/interactiveReferenceGame/results/rscripts/app/server.R
+++ experiments/elisa_paper_relevant/interactiveReferenceGame/results/rscripts/app/server.R
@@ -7,6 +7,7 @@ library(shiny)
 
 setwd("/Users/elisakreiss/Documents/Stanford/overinformativeness/experiments/elisa_paper_relevant/interactiveReferenceGame/results/rscripts/app")
 source("helpers.r")
+theme_set(theme_bw(18))
 # runApp("rscripts/app")
 
 # df = read.table(file="data/trialCSV.csv",sep=";", header=T, quote="")
@@ -21,24 +22,31 @@ shinyServer(
   function(input, output) {
     dat <- reactive({
       test <- df[df$alpha == input$alpha & df$colorCost == input$colorcost & df$typeCost == input$typecost & df$lengthWeight == input$lengthWeight & df$typWeight == input$typWeight,]
-      # print(test)
+      # print(levels(test$uttType))
    })
     output$plot2<-renderPlot({
       ggplot(dat(), aes(x=Typicality,y=modelPrediction,color=uttType)) +
-        geom_point(size=.5) +
-        geom_smooth(method="lm",size=.6) +
+        geom_point(size=1) +
+        geom_smooth(method="lm",size=1.3) +
         #geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
         facet_wrap(~condition) +
-        scale_color_manual(values=c("green", "turquoise", "red")) +
+        ylim(0,1) +
+        #green, turquoise, red
+        scale_color_manual(name="Utterance",
+                           breaks=c("typeOnly", "colorOnly", "colorType"),
+                           labels=c("Only Type", "Only Color", "Color + Type"),
+                           values=c("#799938", "#5bc2b7", "#ef6666")) +
+        theme(axis.title=element_text(size=14,colour="#757575")) +
         theme(axis.title=element_text(size=14,colour="#757575")) +
         theme(axis.text.x=element_text(size=10,colour="#757575")) +
         theme(axis.text.y=element_text(size=10,colour="#757575")) +
         theme(axis.ticks=element_line(size=.25,colour="#757575"), axis.ticks.length=unit(.75,"mm")) +
-        theme(strip.text.x=element_text(size=12,colour="#757575")) +
-        theme(legend.title=element_text(size=14,color="#757575")) +
-        theme(legend.text=element_text(size=11,colour="#757575")) +
+        theme(strip.text.x=element_text(size=18,colour="#757575")) +
+        theme(legend.position="top",legend.box="horizontal") +
+        theme(legend.title=element_text(size=15,color="#757575")) +
+        theme(legend.text=element_text(size=12,colour="#757575")) +
         theme(strip.background=element_rect(colour="#939393",fill="white")) +
         theme(panel.background=element_rect(colour="#939393"))
-    },height = 400,width = 600)
+    },height = 400,width = 550)
   }
 )
--- experiments/elisa_paper_relevant/interactiveReferenceGame/results/rscripts/app/ui.R
+++ experiments/elisa_paper_relevant/interactiveReferenceGame/results/rscripts/app/ui.R
@@ -1,64 +1,34 @@
 shinyUI(fluidPage(
   titlePanel("Overinformativeness Model Visualizations"),
-  
+
   sidebarLayout(
     sidebarPanel(
-      helpText("overinformativeness model visualizations"),
-      
+      h4("Parameters"),
+      helpText("Set parameters"),
+
       sliderInput("alpha",
                   label = "Alpha:",
-                  min = 0, max = 18, value = 6, step = 2),
-      
+                  min = 0, max = 18, value = 18, step = 2),
+
       sliderInput("colorcost",
                   label = "Cost for mentioning color:",
                   min = -3, max = 3, value = 0, step = 1.5),
-      
+
       sliderInput("typecost",
                   label = "Cost for mentioning type:",
-                  min = -3, max = 3, value = 0, step = 1.5),
-      
+                  min = -3, max = 3, value = -1.5, step = 1.5),
+
       sliderInput("lengthWeight",
                   label = "LengthWeight:",
                   min = 0, max = 1, value = 0, step = 0.25),
-      
+
       sliderInput("typWeight",
-                  label = "TypWeight",
-                  min = 0, max = 10, value = 0, step = 2)
+                  label = "TypicalityWeight",
+                  min = 0, max = 10, value = 6, step = 2)
     ),
-    
-    mainPanel(plotOutput("plot2"))
+
+    mainPanel(
+      plotOutput("plot2"),
+      img(src='utterance_by_conttyp_poster.png', height = 400)
   )
-))
-
-# shinyUI(fluidPage(
-#   titlePanel("Overinformativeness Model Visualizations"),
-# 
-#   sidebarLayout(
-#     sidebarPanel(
-#       helpText("overinformativeness model visualizations"),
-# 
-#       sliderInput("alpha",
-#                   label = "Alpha:",
-#                   min = 0, max = 18, value = 6, step = 2),
-# 
-#       sliderInput("colorcost",
-#                   label = "Cost for mentioning color:",
-#                   min = -3, max = 3, value = 0, step = 1.5),
-# 
-#       sliderInput("typecost",
-#                   label = "Cost for mentioning type:",
-#                   min = -3, max = 3, value = 0, step = 1.5),
-# 
-#       sliderInput("lengthWeight",
-#                   label = "LengthWeight:",
-#                   min = 0, max = 1, value = 0, step = 0.25),
-# 
-#       sliderInput("typWeight",
-#                   label = "TypWeight",
-#                   min = 0, max = 10, value = 0, step = 2)
-#       ),
-# 
-#     mainPanel(plotOutput("plot2"))
-#   )
-# ))
-
+)))
