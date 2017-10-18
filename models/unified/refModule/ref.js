d4e5472093821cf03ec489b98134aa800c7ce2ae
hawkrobe <rxdh@stanford.edu>
2017-10-18 15:23:56 -0700

    Revert colorSize model to operate on reduced version

--- models/unified/refModule/ref.js
+++ models/unified/refModule/ref.js
@@ -26,35 +26,21 @@ var getTypicalityUtterances = function(context) {
   })));
 };
 
+// utterances are only modifiers
 var getColorSizeUtterances = function(context) {
   return _.uniq(_.flattenDeep(_.map(context, function(obj) {
-    return _.map(powerset([obj.size, obj.color]), function(modifier) {
-      return _.map([obj.item], function(noun) {
-	return modifier.concat(noun).join('_');
-      });
-    });
+    return _.map(_.filter(powerset([obj.size, obj.color]), v => v != ''),
+		 modifier => modifier.join('_'));
+      // return _.map([obj.item], function(noun) {
+    //return modifier.join('_');
+      // });
   })));
 };
 
-var colors = ["yellow", "orange", "red", "pink", "green",
-              "purple", "white", "blue", "brown", "black"];
-
-var reduced_colors = ["color", "othercolor"];              
-
-var sizes = ["big", "small"];
-
-var reduced_sizes = ["size", "othersize"];
-
-var types = ["fan", "tv", "desk", "couch", "desk", "chair", "couch",
-	     'belt',  'flower',  'yarn',  'turtle',  'butterfly',
-	     'tack',  'billiardball',  'jacket',  'coathanger',  'candle',
-	     'binder',  'ornament',  'phone',  'weddingcake',  'toothbrush',
-	     'bracelet',  'pepper',  'golfball',  'cap',  'teacup',  'avocado',
-	     'hairdryer',  'shoe',  'bucket',  'rug',  'comb',  'guitar',
-	     'stapler',  'cushion',  'chair',  'book',  'bike',  'rock',  'napkin',
-	     'balloon', 'framee'];
-
-var reduced_types = ['item'];       
+// Working with collapsed color utterances
+var colors = ['color', 'othercolor'];
+var sizes = ["size", "othersize"];
+var types = ['item'];       
 
 var makeArr = function(n, v) {
   return _.repeat(n, v);
@@ -68,21 +54,7 @@ var makeColorSizeLists = function(wordsOrObjects) {
   return _.flattenDepth(_.map(sizeList, function(size) {
     return _.map(colorList, function(color) {
       return _.map(typeList, function(type) {
-        return [size, color, type]
-      });
-    });
-  }), 2);
-};
-
-var makeColorSizeReducedLists = function(wordsOrObjects) {
-  var colorList = wordsOrObjects === 'words' ? reduced_colors.concat('') : reduced_colors;
-  var sizeList = wordsOrObjects === 'words' ? reduced_sizes.concat('') : reduced_sizes;
-  var typeList = wordsOrObjects === 'words' ? reduced_types.concat('') : reduced_types;
-
-  return _.flattenDepth(_.map(sizeList, function(size) {
-    return _.map(colorList, function(color) {
-      return _.map(typeList, function(type) {
-        return [size, color, type]
+        return [size, color, type];
       });
     });
   }), 2);
@@ -93,15 +65,6 @@ var colorSizeWordMeanings = function(params) {
     _.zipObject(colors, _.times(colors.length, _.constant(params.colorTyp))),
     _.zipObject(sizes, _.times(sizes.length, _.constant(params.sizeTyp))),
     _.zipObject(types, _.times(types.length, _.constant(1))),
-    {'thing' : 1}
- );
-};
-
-var colorSizeReducedWordMeanings = function(params) {
-  return _.extend(
-    _.zipObject(reduced_colors, _.times(reduced_colors.length, _.constant(params.colorTyp))),
-    _.zipObject(reduced_sizes, _.times(reduced_sizes.length, _.constant(params.sizeTyp))),
-    _.zipObject(reduced_types, _.times(reduced_types.length, _.constant(1))),
     {'thing' : 1}
  );
 };
@@ -124,23 +87,6 @@ var constructLexicon = function(params) {
 	return _.reduce(meanings, _.multiply);
       }));
     }));
-  } else if (params.modelVersion === 'colorSizeReduced') {
-    var allUtts = makeColorSizeReducedLists('words');
-    var uttKeys = _.map(allUtts, (utt) => _.filter(utt, u => u != '').join('_'));
-    var allObjs = makeColorSizeReducedLists('objects');
-    var objKeys = _.map(allObjs, (obj) => obj.join('_'));
-    var wordMeaning = colorSizeWordMeanings(params);
-    return _.zipObject(uttKeys, _.map(allUtts, function(utt) {
-      return _.zipObject(objKeys, _.map(allObjs, function(obj) {
-  var meanings = _.map(_.zip(utt, obj), function(tuple) {
-          var uttWord = tuple[0], objProp = tuple[1];
-          return (uttWord === '' ? 1 :
-                  (uttWord === 'thing' || uttWord === objProp) ? wordMeaning[uttWord] :
-                  (1 - wordMeaning[uttWord]));
-  });
-  return _.reduce(meanings, _.multiply);
-      }));
-    }));   
   } else if (params.modelVersion === 'typicality') {
     return require('./json/typicality-meanings.json');
   } else if (params.modelVersion === 'nominal') {
@@ -266,8 +212,8 @@ var getRelativeLength = function(params, label) {
 module.exports = {
   getNominalUtterances, getColorSizeUtterances, getTypicalityUtterances,
   constructLexicon, powerset, getSubset, 
-  bayesianErpWriter, writeERP, writeCSV,
+  bayesianErpWriter, writeERP, writeCSV, readCSV,
   getData, getConditions,
   getRelativeLength, getRelativeLogFrequency, getTypSubset,
-  colors, sizes, types
+  colors, sizes
 };
