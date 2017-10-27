351f7932e3d8b05fedd5e5b4d5c84aa026d28697
Elisa Kreiss <elisakreiss@yahoo.de>
2017-10-27 19:34:21 +0200

    purple frequencies and lengths

--- writing/2017/cogsci/corpus_counts/getGoogleFreqs.R
+++ writing/2017/cogsci/corpus_counts/getGoogleFreqs.R
@@ -3,7 +3,7 @@ library(jsonlite)
 
 # search single-word utteranes (for some reason you need to do this in two steps for correct results)
 d1 <- ngram(terms=c("apple","avocado","banana","carrot","pear","pepper","tomato","black","blue","brown","green","orange"))
-d2 <- ngram(terms=c("pink","red","yellow"),yr.start=1960)
+d2 <- ngram(terms=c("purple","red","yellow"),yr.start=1960)
 d1 = as.data.frame(colSums(d1[,-length(colnames(d1))]))
 d2 = as.data.frame(colSums(d2[,-length(colnames(d2))]))
 colnames(d1) = c("Frequency")
@@ -12,7 +12,7 @@ d = rbind(d1,d2)
 d$Utterance = row.names(d)
 
 # search two-word utterances (for some reason it returns an error when a term isn't found, so you need to include some error handling to just return zero when necessary)
-pairs = c("black avocado","black pepper","blue apple","blue banana","brown banana","brown carrot","green apple","green avocado","green pear","green pepper","green tomato","orange carrot","orange pear","orange pepper","pink carrot","pink tomato","red apple","red avocado","red pepper","red tomato","yellow banana","yellow pear")
+pairs = c("black avocado","black pepper","blue apple","blue banana","brown banana","brown carrot","green apple","green avocado","green pear","green pepper","green tomato","orange carrot","orange pear","orange pepper","purple carrot","purple tomato","red apple","red avocado","red pepper","red tomato","yellow banana","yellow pear")
 
 for (p in pairs) {
  out = tryCatch(
@@ -46,12 +46,13 @@ freqs = d %>%
   select(Utterance,logFrequency) %>%
   spread(Utterance,logFrequency)
 
-write(toJSON(freqs, pretty=TRUE),file="/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/10_bda_comparison/refModule/json/frequencies.json")
+# write(toJSON(freqs, pretty=TRUE),file="/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/10_bda_comparison/refModule/json/frequencies.json")
+write(toJSON(freqs, pretty=TRUE),file="/Users/elisakreiss/Documents/Stanford/overinformativeness/models/old/11_visualization/refModule/json/frequencies.json")
 
-lengths = data.frame(Utterance = c("black_avocado","black_pepper","blue_apple","blue_banana","brown_banana","brown_carrot","green_apple","green_avocado","green_pear","green_pepper","green_tomato","orange_carrot","orange_pear","orange_pepper","pink_carrot","pink_tomato","red_apple","red_avocado","red_pepper","red_tomato","yellow_banana","yellow_pear","apple","avocado","banana","carrot","pear","pepper","tomato","black","blue","brown","green","orange","pink","red","yellow"))
+lengths = data.frame(Utterance = c("black_avocado","black_pepper","blue_apple","blue_banana","brown_banana","brown_carrot","green_apple","green_avocado","green_pear","green_pepper","green_tomato","orange_carrot","orange_pear","orange_pepper","purple_carrot","purple_tomato","red_apple","red_avocado","red_pepper","red_tomato","yellow_banana","yellow_pear","apple","avocado","banana","carrot","pear","pepper","tomato","black","blue","brown","green","orange","purple","red","yellow"))
 lengths$Length = nchar(as.character(lengths$Utterance))
 lengths = lengths %>%
   spread(Utterance,Length)
 
-write(toJSON(lengths,pretty=TRUE), file="/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/models/10_bda_comparison/refModule/json/lengths.json")
+write(toJSON(lengths,pretty=TRUE), file="/Users/elisakreiss/Documents/Stanford/overinformativeness/models/old/11_visualization/refModule/json/lengths.json")
                      
