411e00ee6cca823a62e154d5288e94de1c9728d4
hawkrobe <rxdh@stanford.edu>
2018-03-22 10:31:08 -0700

    Update preprocessing a bit

--- models/unified/bdaInput/preprocess_typicality.js
+++ models/unified/bdaInput/preprocess_typicality.js
@@ -2,17 +2,17 @@ var fs = require('fs');
 var _ = require('lodash');
 var refModule = require('../refModule');
 
-var data = refModule.readCSV("bda_data_typicality.csv"); // loads data with 46 "other utterance" cases excluded
+var data = require("./bda_data_typicality.json"); // loads data with 46 "other utterance" cases excluded
 console.log(data);
 //console.log("Loading data complete...",+data.length+" data points");
 
-var conditions = refModule.readCSV("./unique_conditions_typicality.csv"); 
+var conditions = require("./unique_conditions_typicality.json"); 
 
 var contexts = _.map(conditions, function(c) {
   return _.extend(c, {context: [
     {color: c.t_color, item: c.t_item},
     {color: c.d1_color, item: c.d1_item},
-    {color: c.d2_color, item: c.d2_color}
+    {color: c.d2_color, item: c.d2_item}
   ]});
 });
 
